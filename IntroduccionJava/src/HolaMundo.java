6f7c9c751bdb3287862bac7a6bb47971775da613
FedeZet <fcoelhovazquez@gmail.com>
2024-07-29 12:16:41 -0300

    Introducción a Java

---
+++ IntroduccionJava/src/HolaMundo.java
@@ -0,0 +1,8 @@
+public class HolaMundo {
+    // Agregamos el método principal (main)
+    public static void main(String[] args) {
+        // Esta es la manera de escribir mensajes en consola:
+        System.out.println("Hola Mundo con Java");
+        System.out.println("Saludos");
+    }
+}
---
+++ IntroduccionJava/src/Presentate.java
@@ -0,0 +1,8 @@
+public class Presentate {
+    public static void main(String[] args) {
+        // Reto 1 - Preséntate en Java
+        System.out.println("Nombre: Federico Coelho");
+        System.out.println("Edad: 24 años");
+        System.out.println("País: Uruguay");
+    }
+}
