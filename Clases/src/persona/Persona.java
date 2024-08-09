9cc4e48cb15c4f59a1829ece42bcf5122ff3d4df
FedeZet <fcoelhovazquez@gmail.com>
2024-08-09 13:51:55 -0300

    Clases - Clase Object, Método toString, Contexto Estático y ejemplos

--- Clases/src/persona/Persona.java
+++ Clases/src/persona/Persona.java
@@ -1,10 +1,13 @@
 package persona;
 
+// Todas las clases de Java heredan de manera directa o indirecta de la clase Object
 public class Persona {
     // Reto - Encapsular la clase Persona
 
     // Atributos
     // Se debe poner el tipo de dato. No se puede usar var
+    private static int contadorPersonas = 0; // Atributo estático: No se asocia a los objetos. Solamente a la clase
+    private int idPersona;
     private String nombre; // Valor por default null
     private String apellido;
 
@@ -12,12 +15,26 @@ public class Persona {
     public Persona(String nombre, String apellido) {
         this.nombre = nombre;
         this.apellido = apellido;
+        // Asignamos id único con ayuda de la variable estática
+        // Incrementar el atributo estático (static)
+        this.idPersona = ++Persona.contadorPersonas;
+    }
+
+    // Sobreescribimos el método toString() de la clase Object
+    @Override
+    public String toString() {
+        return "Id: " + this.idPersona + ", Nombre: " + this.nombre + ", Apellido: " + this.apellido +
+                ", Dir. Mem.: " + super.toString(); // Llamando al método desde la clase Padre (Object)
     }
 
     // Métodos
     public void mostrarPersona() {
         System.out.println("Nombre: " + this.nombre);
         System.out.println("Apellido: " + this.apellido);
+    }
+
+    public int getIdPersona() {
+        return this.idPersona;
     }
 
     public String getNombre() {
@@ -32,5 +49,9 @@ public class Persona {
     }
     public void setApellido(String apellido) {
         this.apellido = apellido;
+    }
+
+    public static int getContadorPersonas() {
+        return Persona.contadorPersonas;
     }
 }
--- Clases/src/persona/pruebaPersona.java
+++ Clases/src/persona/pruebaPersona.java
@@ -4,14 +4,28 @@ public class pruebaPersona {
     public static void main(String[] args) {
         System.out.println("*** Creación de Clase y Objetos de tipo Persona ***");
 
+        System.out.println("Variable estática: " + Persona.getContadorPersonas());
+
+        System.out.println(); // Salto de línea
+
         // Primer objeto
         var objeto1 = new Persona("Layla", "Acosta"); // Reserva espacio en memoria del tipo de la clase Persona
         objeto1.mostrarPersona(); // Llamamos al método
 
+        System.out.println("Variable estática: " + Persona.getContadorPersonas());
         System.out.println(); // Salto de línea
 
         // Segundo objeto
         var objeto2 = new Persona("Ian", "Gomez"); // Instanciar la clase
         objeto2.mostrarPersona();
+
+        System.out.println("Variable estática: " + objeto2.getContadorPersonas());
+        // Esto NO es buena práctica, se debe llamar al método estático desde la clase y no desde el objeto
+
+        System.out.println("\n*** Método toString() ***");
+        // Utilizando el método sobreescrito toString() de la clase Object
+        // System.out.println(objeto1.toString());
+        System.out.println(objeto1); // Automáticamente, se llama al método toString()
+        System.out.println(objeto2);
     }
 }
